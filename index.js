c0af3c7dc6234be84b2558a65d0410df7736997c
Sabeeh098 <sabeehup@gmail.com>
2024-01-25 05:32:14 +0530

    update mongodb

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose')
-mongoose.connect('mongodb://127.0.0.1:27017/UserManagement',{
+mongoose.connect('mongodb+srv://kavyareddy:kavyareddy@cluster0.msabz.mongodb.net/ecommerce',{
   useNewUrlParser: true,
  useUnifiedTopology: true,
 })
